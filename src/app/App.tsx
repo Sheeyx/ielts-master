213d221b616284c4aa4ab60d76fd5e36b5b69940
Sheeyx <sheeyx00@gmail.com>
2024-05-20 12:15:47 +0900

    feat: develop banner & header sections

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Switch, useLocation } from "react-router-dom";
+import Header from './components/header';
+import { Banner } from './components/banner';
 
 
 function App() {
   return (
     <div className="app">
-        <Switch>
-          <Route path="/">
-            Home
-          </Route>
+      <Banner />
+      <Header/>
+      <Switch>
+        <Route path="/">
+          Home
+        </Route>
       </Switch>
     </div>
   );
---
+++ src/app/components/banner/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.scss';
+
+export const Banner = () =>{
+    return (
+        <div className={"banner"}>
+            Prepare for IELTS with us
+        </div>
+    )
+}
